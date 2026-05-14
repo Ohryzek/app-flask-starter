d34455445175622760e8c113eb7b2edb5c7e3b20
Ondřej Čáslavský <ondrahrajehryp@gmail.com>
2026-05-14 15:45:25 +0200

    oprava2

--- app.py
+++ app.py
@@ -6,43 +6,42 @@ app = Flask(__name__)
 
 SPRAVNE_HESLO = "tajneheslo"
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-  
+    # aktuální datum
     date = datetime.now().strftime("%d. %m. %Y")
+    
+..
+    
     return render_template("page.html", date=date)      
 
 @app.route("/pozdrav-post", methods=["POST", "GET"])
 def pozdrav_post():
+    # aktuální datum
     date = datetime.now().strftime("%d. %m. %Y")
     error = None
     name = None
     surname = None
-    secret_info = None
+    secret_info = None  
 
     if request.method == "POST":
         name = request.form.get("name")
         surname = request.form.get("surname")
-        password = request.form.get("password")
+        password = request.form.get("password") 
 
-   
+      
+        if password == SPRAVNE_HESLO:
+            secret_info = "Tohle je přísně tajná informace! Obědy na Gymzr jsou super."
+        elif password:
+            error = "Chyba: Zadané heslo je nesprávné!"
+
+  
         if not name or name.strip() == "":
             error = "Chyba: Zadej prosím své jméno, pole nesmí zůstat prázdné."
             name = None
         elif len(name) > 50:
             error = "Chyba: Zadané jméno je příliš dlouhé (maximálně 50 znaků)."
             name = None
-
-     
-        if password:
-            if password == SPRAVNE_HESLO:
-                secret_info = "Tohle je přísně tajná informace! Obědy na naší škole jsou super."
-            else:
-              
-                if error:
-                    error += " A navíc je zadané heslo nesprávné!"
-                else:
-                    error = "Chyba: Zadané heslo je nesprávné!"
 
     return render_template("pozdrav-post.html", date=date, name=name, surname=surname, error=error, secret_info=secret_info)
 
